73605622d0059fd45b6f0ed0d0f650399aa81ac3
yhassanzadeh13 <yhassanzadeh13@ku.edu.tr>
2021-10-06 07:02:29 -0700

    refactors underlay

--- network/underlay.go
+++ network/underlay.go
@@ -2,7 +2,7 @@ package network
 
 import "github/yhassanzadeh13/skipgraph-go/model/skipgraph"
 
-type Network interface {
+type Underlay interface {
 	// Start starts the networking layer.
 	Start() <-chan interface{}
 
