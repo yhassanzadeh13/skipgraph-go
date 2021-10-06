9933304556c6e78f43a8156b6a680b0bea182ed9
yhassanzadeh13 <yhassanzadeh13@ku.edu.tr>
2021-10-06 06:48:25 -0700

    adds network interface

--- network/network.go
+++ network/network.go
@@ -1,1 +1,17 @@
 package network
+
+import "github/yhassanzadeh13/skipgraph-go/model/skipgraph"
+
+type Network interface {
+	// Start starts the networking layer.
+	Start() <-chan interface{}
+
+	// Stop stops the networking layer.
+	Stop() <-chan interface{}
+
+	// SetMessageHandler determines the handler of a message based on its message type.
+	SetMessageHandler(interface{}, func(interface{}) error) error
+
+	// Send sends a message to a list of target recipients in the underlying network.
+	Send(interface{}, skipgraph.IdentifierList) error
+}
