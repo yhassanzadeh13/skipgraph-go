87b66217945a26bc89eefef695deb7724670afad
Sanaz <staheri14@ku.edu.tr>
2021-10-11 07:18:41 -0700

    adds the implementation of a mocked underly

--- unittest/mocknet/underlay.go
+++ unittest/mocknet/underlay.go
@@ -1,23 +1,39 @@
 package mocknet
 
-import "github/yhassanzadeh13/skipgraph-go/model/skipgraph"
+import (
+	"errors"
+)
 
 type mockUnderlay struct {
 	// it behaves the same for all the sharing skip graph nodes
 	// there is only one handler per message type (but not per caller)
-	MessageHandlers map[interface{}]func(interface{}) error
+	messageHandlers map[interface{}]func(interface{}) error
 
 }
 
+// newMockUnderlay is the constructor for MockUnderlay object
+func newMockUnderlay(messageHandlers map[interface{}]func(interface{}) error) *mockUnderlay {
+	return &mockUnderlay{messageHandlers: messageHandlers}
+}
+
+
 // SetMessageHandler determines the handler of a message based on its message type.
-func (m mockUnderlay) SetMessageHandler(node skipgraph.Identifier, message interface{}, handler func(interface{}) error) error {
-	// TODO should distinguish between distinct identifiers
-	m.MessageHandlers[message] = handler
+func (m mockUnderlay) SetMessageHandler(message interface{}, handler func(interface{}) error) error {
+	m.messageHandlers[message] = handler
 	return nil
 }
 
 // Send sends a message to a list of target recipients in the underlying network.
-func (m mockUnderlay) Send(interface{}, skipgraph.IdentifierList) error {
-	return nil
+func (m mockUnderlay) Send(message interface{}) error {
+	// check the support of the supplied message
+	handler := m.messageHandlers[message]
+	if handler == nil{
+		return errors.New("no handler for the supplied message")
+	}
 
+	// call the installed handler
+	handler(message)
+
+	// no error occurred
+	return nil
 }
