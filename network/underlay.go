e31beac122f3edb8cb11d58e9759a8a77a5d7664
Sanaz <staheri14@ku.edu.tr>
2021-10-12 09:33:59 -0700

    adds  MessageHandler and updates underlay interface argument types

--- network/underlay.go
+++ network/underlay.go
@@ -1,18 +1,23 @@
 package network
 
-import "github/yhassanzadeh13/skipgraph-go/model/skipgraph"
+import (
+	"github/yhassanzadeh13/skipgraph-go/model/messages"
+	"github/yhassanzadeh13/skipgraph-go/model/skipgraph"
+)
 
 // Underlay represents the underlying network for which skip graph node is interacting with.
 type Underlay interface {
 	// Start starts the networking layer.
-	//Start() <-chan interface{}
+	Start() <-chan interface{}
 
 	// Stop stops the networking layer.
-	//Stop() <-chan interface{}
+	Stop() <-chan interface{}
 
 	// SetMessageHandler determines the handler of a message based on its message type.
-	SetMessageHandler(interface{}, func(interface{}) error) error
+	SetMessageHandler(messages.MessageType, MessageHandler) error
 
 	// Send sends a message to a list of target recipients in the underlying network.
 	Send(interface{}, skipgraph.IdentifierList) error
 }
+
+type MessageHandler func(messages.Message) error
