3b1aaa1ec6fcbda5a70a01699fa41e8d80a7d316
Sanaz <staheri14@ku.edu.tr>
2022-12-08 17:16:20 -0800

    implements compare method with const outputs

--- model/skipgraph/identifier.go
+++ model/skipgraph/identifier.go
@@ -1,8 +1,14 @@
 package skipgraph
 
-import "encoding/hex"
+import (
+	"bytes"
+	"encoding/hex"
+)
 
 const IdentifierSize = 32
+const CompareEqaul = "compare equal"
+const CompareGreater = "compare greater"
+const CompareLess = "compare less"
 
 // Identifier represents a 32-byte unique identifier a Skip Graph node.
 type Identifier [IdentifierSize]byte
@@ -11,4 +17,17 @@ type IdentifierList []Identifier
 
 func (i Identifier) String() string {
 	return hex.EncodeToString(i[:])
+}
+
+// compare
+func (i Identifier) compare(other Identifier) string {
+	cmp := bytes.Compare(i[:], other[:])
+	switch cmp {
+	case 1:
+		return CompareGreater
+	case -1:
+		return CompareLess
+	default:
+		return CompareEqaul
+	}
 }
