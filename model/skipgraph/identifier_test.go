91db47a1e2c43d587a2065234d1adf70c1e1c8e1
Sanaz Taheri <35961250+staheri14@users.noreply.github.com>
2022-12-13 17:58:08 -0800

    feat: Adds SkipGraph Address and MembershipVector types and  their utilities (#4)
    * adds Address type and its methods 
    
    
    makes minor rewording 
    
    
    adds getters and string methods
    
    
    fixes a typo
    
    converts pointer types to value types
    
    * implements compare method with const outputs
    
    * implements unittest for the identifier comparison
    
    * implements ToIdentifier
    
    * adds MembershipVector and its utils
    
    * tests CommonPrefix
    
    * adds go doc
    
    * updates binary string conversion for MembershipVectors
    
    * adds unit test for toBinaryString for byte data type
    
    * improves docs
    
    * exposes compare method
    
    * separates test packages
    
    * improves documentation and namings
    
    * adds test for ToMEmbershipVector
    
    * adds ending dots
    
    * compares against a new identical identifier

---
+++ model/skipgraph/identifier_test.go
@@ -0,0 +1,25 @@
+package skipgraph_test
+
+import (
+	"github.com/stretchr/testify/require"
+	"github/yhassanzadeh13/skipgraph-go/model/skipgraph"
+	"testing"
+)
+
+// TestCompare checks the correctness of the Identifier comparison function
+func TestCompare(t *testing.T) {
+	s1 := []byte("12")
+	s2 := []byte("22")
+	s3 := []byte("12")
+	i1, err := skipgraph.ToIdentifier(s1)
+	require.NoError(t, err)
+	i2, err := skipgraph.ToIdentifier(s2)
+	require.NoError(t, err)
+	i3, err := skipgraph.ToIdentifier(s3)
+	require.NoError(t, err)
+
+	require.Equal(t, skipgraph.CompareLess, i1.Compare(i2))
+	require.Equal(t, skipgraph.CompareGreater, i2.Compare(i1))
+	require.Equal(t, skipgraph.CompareEqual, i1.Compare(i3))
+
+}
