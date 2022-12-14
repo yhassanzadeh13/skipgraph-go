2bbedd4c31d510aa1030ca89ff39cc32499ded2d
Sanaz <staheri14@ku.edu.tr>
2022-12-13 16:53:29 -0800

    adds unit test for toBinaryString for byte data type

--- model/skipgraph/membershipVector_test.go
+++ model/skipgraph/membershipVector_test.go
@@ -19,8 +19,26 @@ func TestMembershipVectorCompare(t *testing.T) {
 	require.Equal(t, 0, res)
 
 	// create two membershipVectors with non-zero common prefix
-	v1[0] = 254
+	v1[0] = 253
 	res = v1.CommonPrefix(v2)
-	require.Equal(t, 7, res)
+	require.Equal(t, 6, res)
+}
 
+// TestToBinaryString tests correctness of ToBinaryString
+func TestToBinaryString(t *testing.T) {
+	v1 := byte(1) // 00000001
+	s1 := ToBinaryString(v1)
+	require.Equal(t, "00000001", s1)
+
+	v2 := byte(2) // 00000010
+	s2 := ToBinaryString(v2)
+	require.Equal(t, "00000010", s2)
+
+	v3 := byte(128) // 10000000
+	s3 := ToBinaryString(v3)
+	require.Equal(t, "10000000", s3)
+
+	v4 := byte(65) // 01000001
+	s4 := ToBinaryString(v4)
+	require.Equal(t, "01000001", s4)
 }
