ef6e2fe50db49757ec486a66ac5b0b4af03bbd38
Sanaz <staheri14@ku.edu.tr>
2022-12-13 17:05:29 -0800

    separates test packages

--- model/skipgraph/identifier_test.go
+++ model/skipgraph/identifier_test.go
@@ -1,7 +1,8 @@
-package skipgraph
+package skipgraph_test
 
 import (
 	"github.com/stretchr/testify/require"
+	"github/yhassanzadeh13/skipgraph-go/model/skipgraph"
 	"testing"
 )
 
@@ -9,10 +10,12 @@ import (
 func TestCompare(t *testing.T) {
 	s1 := []byte("12")
 	s2 := []byte("22")
-	i1, _ := ToIdentifier(s1)
-	i2, _ := ToIdentifier(s2)
+	i1, err := skipgraph.ToIdentifier(s1)
+	require.NoError(t, err)
+	i2, err := skipgraph.ToIdentifier(s2)
+	require.NoError(t, err)
 
-	require.Equal(t, CompareLess, i1.compare(i2))
-	require.Equal(t, CompareGreater, i2.compare(i1))
-	require.Equal(t, CompareEqaul, i1.compare(i1))
+	require.Equal(t, skipgraph.CompareLess, i1.Compare(i2))
+	require.Equal(t, skipgraph.CompareGreater, i2.Compare(i1))
+	require.Equal(t, skipgraph.CompareEqaul, i1.Compare(i1))
 }
--- model/skipgraph/membershipVector_test.go
+++ model/skipgraph/membershipVector_test.go
@@ -1,19 +1,20 @@
-package skipgraph
+package skipgraph_test
 
 import (
 	"github.com/stretchr/testify/require"
+	"github/yhassanzadeh13/skipgraph-go/model/skipgraph"
 	"testing"
 )
 
 // TestMembershipVectorCompare tests CommonPrefix method
 func TestMembershipVectorCompare(t *testing.T) {
 	// create two membershipVectors with 32 * 8 common prefix
-	v1 := MembershipVector{0}
+	v1 := skipgraph.MembershipVector{0}
 	res := v1.CommonPrefix(v1)
 	require.Equal(t, 256, res)
 
 	// create two membershipVectors with no common prefix
-	v2 := MembershipVector{0}
+	v2 := skipgraph.MembershipVector{0}
 	v2[0] = 255
 	res = v1.CommonPrefix(v2)
 	require.Equal(t, 0, res)
@@ -27,18 +28,18 @@ func TestMembershipVectorCompare(t *testing.T) {
 // TestToBinaryString tests correctness of ToBinaryString
 func TestToBinaryString(t *testing.T) {
 	v1 := byte(1) // 00000001
-	s1 := ToBinaryString(v1)
+	s1 := skipgraph.ToBinaryString(v1)
 	require.Equal(t, "00000001", s1)
 
 	v2 := byte(2) // 00000010
-	s2 := ToBinaryString(v2)
+	s2 := skipgraph.ToBinaryString(v2)
 	require.Equal(t, "00000010", s2)
 
 	v3 := byte(128) // 10000000
-	s3 := ToBinaryString(v3)
+	s3 := skipgraph.ToBinaryString(v3)
 	require.Equal(t, "10000000", s3)
 
 	v4 := byte(65) // 01000001
-	s4 := ToBinaryString(v4)
+	s4 := skipgraph.ToBinaryString(v4)
 	require.Equal(t, "01000001", s4)
 }
