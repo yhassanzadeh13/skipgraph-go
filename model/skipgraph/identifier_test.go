0cfde6b8baa6b73fab31be4c770f6feba9766ed8
Sanaz <staheri14@ku.edu.tr>
2022-12-13 17:43:50 -0800

    compares against a new identical identifier

--- model/skipgraph/identifier_test.go
+++ model/skipgraph/identifier_test.go
@@ -10,12 +10,16 @@ import (
 func TestCompare(t *testing.T) {
 	s1 := []byte("12")
 	s2 := []byte("22")
+	s3 := []byte("12")
 	i1, err := skipgraph.ToIdentifier(s1)
 	require.NoError(t, err)
 	i2, err := skipgraph.ToIdentifier(s2)
 	require.NoError(t, err)
+	i3, err := skipgraph.ToIdentifier(s3)
+	require.NoError(t, err)
 
 	require.Equal(t, skipgraph.CompareLess, i1.Compare(i2))
 	require.Equal(t, skipgraph.CompareGreater, i2.Compare(i1))
-	require.Equal(t, skipgraph.CompareEqual, i1.Compare(i1))
+	require.Equal(t, skipgraph.CompareEqual, i1.Compare(i3))
+
 }
