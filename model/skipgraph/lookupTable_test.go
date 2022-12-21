842ee90884f0638765cedb448895393b94a20a42
Sanaz <staheri14@ku.edu.tr>
2022-12-20 16:59:03 -0800

    updates lookup table test to use fixtures

--- model/skipgraph/lookupTable_test.go
+++ model/skipgraph/lookupTable_test.go
@@ -2,8 +2,8 @@ package skipgraph_test
 
 import (
 	"github.com/stretchr/testify/require"
-	"github/yhassanzadeh13/skipgraph-go/model"
 	"github/yhassanzadeh13/skipgraph-go/model/skipgraph"
+	"github/yhassanzadeh13/skipgraph-go/unittest"
 	"testing"
 )
 
@@ -33,42 +33,27 @@ func TestLookupTable_AddEntry(t *testing.T) {
 
 func TestLookupTable_GetEntry(t *testing.T) {
 	// create an identity
-	id, err := skipgraph.StringToIdentifier("123")
-	require.NoError(t, err)
-	memVec, err := skipgraph.StringToMembershipVector("456")
-	require.NoError(t, err)
-	addr := model.NewAddress("localhost", "1234")
-	identity := skipgraph.NewIdentity(id, memVec, addr)
+	identity := unittest.IdentityFixture(t)
+	identity1 := unittest.IdentityFixture(t)
+	require.NotEqual(t, identity1, identity)
 
-	// create another identity
-	id1, err := skipgraph.StringToIdentifier("111")
-	require.NoError(t, err)
-	memVec1, err := skipgraph.StringToMembershipVector("222")
-	require.NoError(t, err)
-	addr1 := model.NewAddress("localhost1", "5678")
-	identity1 := skipgraph.NewIdentity(id1, memVec1, addr1)
-
-	// create an empty lookup table
-	lt := skipgraph.LookupTable{}
+	// declare an empty lookup table
+	var lt skipgraph.LookupTable
 
 	// add the identity as a left neighbor into the lookup table
-	err = lt.AddEntry(skipgraph.LeftDirection, 12, identity)
+	err := lt.AddEntry(skipgraph.LeftDirection, 12, identity)
 	require.NoError(t, err)
 
-	// check that the inserted identity is retrievable
-	retIdentity, err := lt.GetEntry(skipgraph.LeftDirection, 12)
-	require.Equal(t, id, retIdentity.GetIdentifier())
-	require.Equal(t, memVec, retIdentity.GetMembershipVector())
-	require.Equal(t, addr, retIdentity.GetAddress())
-
-	// add the identity as a left neighbor into the lookup table
+	// add the identity as a right neighbor into the lookup table
 	err = lt.AddEntry(skipgraph.RightDirection, 0, identity1)
 	require.NoError(t, err)
 
 	// check that the inserted identity is retrievable
+	retIdentity, err := lt.GetEntry(skipgraph.LeftDirection, 12)
+	require.Equal(t, identity, retIdentity)
+
+	// check that the inserted identity is retrievable
 	retIdentity1, err := lt.GetEntry(skipgraph.RightDirection, 0)
-	require.Equal(t, id1, retIdentity1.GetIdentifier())
-	require.Equal(t, memVec1, retIdentity1.GetMembershipVector())
-	require.Equal(t, addr1, retIdentity1.GetAddress())
+	require.Equal(t, identity1, retIdentity1)
 
 }
