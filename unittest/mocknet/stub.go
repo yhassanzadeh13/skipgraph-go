1bfec08286f648d753c1abc12d3e29f7f83cc400
Sanaz <staheri14@ku.edu.tr>
2023-01-11 17:50:56 -0800

    adds Error types and their utilities

--- unittest/mocknet/stub.go
+++ unittest/mocknet/stub.go
@@ -1,6 +1,7 @@
 package mocknet
 
 import (
+	"errors"
 	"fmt"
 	"github.com/stretchr/testify/require"
 	"github/yhassanzadeh13/skipgraph-go/model/messages"
@@ -29,7 +30,7 @@ func (n *NetworkStub) NewMockUnderlay(t *testing.T, id skipgraph.Identifier) *Mo
 	_, exists := n.underlays[id]
 	require.False(t, exists, "attempting to create mock underlay for already existing identifier")
 
-	u := newMockUnderlay(n)
+	u := NewMockUnderlay(n)
 	n.underlays[id] = u
 
 	return u
@@ -42,18 +43,72 @@ func (n *NetworkStub) routeMessageTo(msg messages.Message, target skipgraph.Iden
 
 	u, exists := n.underlays[target]
 	if !exists {
-		return fmt.Errorf("no mock underlay exists for %x", target)
+		return &NoMockUnderlayError{identifier: target}
 	}
 
 	h, exists := u.messageHandlers[msg.Type]
 	if !exists {
-		return fmt.Errorf("no handler exists for message type %v", msg.Type)
+		return NoHandlerExistError{msgType: msg.Type}
 	}
 
 	err := h(msg)
 	if err != nil {
-		return fmt.Errorf("mock underlay handler could not handler message %w", err)
+		return CouldNotHandleMessageError{msg: msg, id: target}
 	}
 
 	return nil
+}
+
+/*
+========================== error types ========================================
+*/
+
+// NoMockUnderlayError indicates absence of a mock underlay for a node.
+type NoMockUnderlayError struct {
+	identifier skipgraph.Identifier
+}
+
+// Error implements the Error interface for NoMockUnderlayError.
+func (e NoMockUnderlayError) Error() string {
+	return fmt.Sprintf("no mock underlay exists for %x", e.identifier)
+}
+
+// isCouldNotHandleMessageError checks whether err is of the type CouldNotHandleMessageError.
+func isCouldNotHandleMessageError(err error) bool {
+	return errors.As(err, &CouldNotHandleMessageError{})
+}
+
+// ======================
+
+// NoHandlerExistError indicates absence of a handler for a message type.
+type NoHandlerExistError struct {
+	msgType string
+}
+
+// Error implements the Error interface for NoHandlerExistError.
+func (e NoHandlerExistError) Error() string {
+	return fmt.Sprintf("no handler exists for message type %v", e.msgType)
+}
+
+// IsNoHandlerExistError checks whether err is of type NoHandlerExistError.
+func IsNoHandlerExistError(err error) bool {
+	return errors.As(err, &NoHandlerExistError{})
+}
+
+// ======================
+
+// CouldNotHandleMessageError indicates that a message could not be handled by a message handler.
+type CouldNotHandleMessageError struct {
+	msg messages.Message
+	id  skipgraph.Identifier
+}
+
+// Error implements the Error interface for CouldNotHandleMessageError.
+func (e CouldNotHandleMessageError) Error() string {
+	return fmt.Sprintf("the message handler of mock underlay of node %v could not handle message %v", e.id, e.msg)
+}
+
+// IsCouldNotHandleMessageError  checks whether err is of type CouldNotHandleMessageError.
+func IsCouldNotHandleMessageError(err error) bool {
+	return errors.As(err, &CouldNotHandleMessageError{})
 }
