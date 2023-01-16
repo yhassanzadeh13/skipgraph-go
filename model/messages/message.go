c5058869802750d44a0c6ad5b74605672d44488e
Sanaz <staheri14@ku.edu.tr>
2023-01-16 08:01:21 -0800

    introduces Type enums

--- model/messages/message.go
+++ model/messages/message.go
@@ -3,6 +3,13 @@ package messages
 // Type determines the type of an underlay network message
 type Type string
 
+const (
+	// GetEntry is a message type for requesting an entry from a node.
+	GetEntry = Type("Get Entry")
+	// AddEntry is a message type for adding an entry to a node.
+	AddEntry = Type("Add Entry")
+)
+
 // Message is an underlay network message
 type Message struct {
 	// Type indicates type of the0 message
