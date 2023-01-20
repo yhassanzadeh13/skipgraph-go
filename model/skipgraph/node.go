2f6ba8192d0f5aa09b67b70fd931b0ca213f505e
Sanaz <staheri14@ku.edu.tr>
2023-01-20 07:00:42 -0800

    implements JSON to LookUpQuery conversions and node's Start internal logic

--- model/skipgraph/node.go
+++ model/skipgraph/node.go
@@ -16,28 +16,65 @@ type node struct {
 	Underlay network.Underlay
 }
 
-// JSONtoIdentity converts a JSON string to an Identity.
-func JSONtoIdentity(data string) (Identity, error) {
+// LookupQuery holds a node Identity and its metadata reflecting its location in a lookup table.
+type LookupQuery struct {
+	Identity
+	EntryPosition
+}
+type EntryPosition struct {
+	Direction
+	Level
+}
+
+// JSONToLookUPQuery parses a JSON string to a LookupQuery struct.
+// returns error if the string is not a valid JSON.
+func JSONToLookUPQuery(data string) (LookupQuery, error) {
 	// convert to byte slice
 	b := []byte(data)
 	// unmarshal byte slice to identity
-	var i Identity
+	var i LookupQuery
 	err := json.Unmarshal(b, &i)
 	if err != nil {
-		return Identity{}, fmt.Errorf("could not parse to Identity %v", err)
+		return LookupQuery{}, fmt.Errorf("could not parse to Entry Query %v", err)
 	}
 	return i, nil
 }
-func (n *node) addLookUpEntry(message messages.Message) error {
-	// parse message payload to a lookup entry.
-	id, error := JSONtoIdentity(message.Payload)
 
+// LookUpQueryToJSON converts a lookup query to a JSON string.
+func LookUpQueryToJSON(i LookupQuery) (string, error) {
+	b, err := json.Marshal(i)
+	if err != nil {
+		return "", fmt.Errorf("could not encode query %v", err)
+	}
+	s := string(b)
+	return s, nil
+}
+
+// addLookUpEntry extracts the lookup query embedded in the message payload and adds the entry to the lookup table.
+func (n *node) addLookUpEntry(message messages.Message) error {
+	// parse message payload to string.
+	qJsonStr, ok := message.Payload.(string)
+	if !ok {
+		return fmt.Errorf("message payload is not a string")
+	}
+	// parse the message payload as a lookup query.
+	query, err := JSONToLookUPQuery(qJsonStr)
+	if err != nil {
+		return fmt.Errorf("could not parse payload to identity %s", qJsonStr)
+	}
 	// add the entry to the lookup table.
+	n.LookupTable.AddEntry(query.Direction, query.Level, query.Identity)
 
 	return nil
 }
 func (n *node) Start() {
 	// set up node's underlay.
-	n.Underlay.SetMessageHandler(messages.AddEntry)
+	// add a handler for add lookup entry `AddEntry` message type.
+	n.Underlay.SetMessageHandler(messages.AddEntry, n.addLookUpEntry)
+	n.Underlay.Start()
+}
 
+func (n *node) Stop() {
+	// stop the node's underlay.
+	n.Underlay.Stop()
 }
