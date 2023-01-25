703c0a17ac885fd54aefe61c8b97f532344f86e7
Sanaz <staheri14@ku.edu.tr>
2023-01-25 07:01:36 -0800

    refactors the node package

--- node/node_test.go
+++ node/node_test.go
@@ -1,21 +1,22 @@
-package skipgraph_test
+package node_test
 
 import (
 	"github.com/stretchr/testify/require"
 	"github/yhassanzadeh13/skipgraph-go/model/skipgraph"
+	"github/yhassanzadeh13/skipgraph-go/node"
 	"github/yhassanzadeh13/skipgraph-go/unittest"
 	"testing"
 )
 
 func TestLookUpQueryToJSON(t *testing.T) {
 	// create a LookupQuery and covert it to JSON
-	lq := skipgraph.LookupQuery{unittest.IdentityFixture(t), skipgraph.EntryPosition{skipgraph.LeftDirection, unittest.LevelFixture(t)}}
- 	jsonStr, err := skipgraph.LookUpQueryToJSON(lq)
+	lq := node.LookupQuery{unittest.IdentityFixture(t), node.EntryPosition{skipgraph.LeftDirection, unittest.LevelFixture(t)}}
+ 	jsonStr, err := node.LookUpQueryToJSON(lq)
 	 require.NoError(t, err)
 	 require.NotEmpty(t, jsonStr)
 
 	 // convert the JSON back to a LookupQuery
-	 lq2, err := skipgraph.JSONToLookUPQuery(jsonStr)
+	 lq2, err := node.JSONToLookUPQuery(jsonStr)
 	 require.NoError(t, err)
 	 require.Equal(t, lq, lq2)
 }
