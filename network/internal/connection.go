3f8dbcf8b3e7fa6cbe69f2e9ce4ffe23640bc765
Yahya <yhassanzadeh@ieee.org>
2024-08-03 17:40:57 -0700

    revises errors doc on connection manager

--- network/internal/connection.go
+++ network/internal/connection.go
@@ -16,16 +16,18 @@ type Connection interface {
 	// Send is a blocking operation, and it will block until the message is sent.
 	// It returns an error if the message could not be sent.
 	// It returns io.EOF if the connection is closed.
+	// Errors from this method are expected to be treated as benign, i.e., don't panic on error.
 	Send([]byte) error
 
 	// Next returns the next message received from the remote peer.
 	// It is a blocking operation, and it will block until a message is received.
 	// It returns io.EOF if the connection is closed.
 	// It returns an error if the message could not be read.
+	// Errors from this method are expected to be treated as benign, i.e., don't panic on error.
 	Next() ([]byte, error)
 
 	// Close gracefully closes the connection. Blocking until the connection is closed.
-	// No error is expected under normal circumstances.
+	// Errors from this method can be considered as benign, i.e., don't panic on error.
 	Close() error
 }
 
@@ -34,9 +36,10 @@ type ConnectionManager interface {
 	// Connect establishes a connection to a remote peer and locally caches and returns the connection.
 	// If the connection is already established, it returns the cached connection.
 	// The cardinal assumption is there is always at most one connection to a remote peer.
+	// Errors from this method are expected to be treated as benign, i.e., don't panic on error.
 	Connect(context.Context, skipgraph.Identifier) (Connection, error)
 
 	// Close closes all connections.
-	// No error is expected under normal circumstances.
+	// Errors from this method can be considered as benign, i.e., don't panic on error.
 	Close() error
 }
