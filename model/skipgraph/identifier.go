dd626971ba5cde7b6725349e85f2d2602500107a
Sanaz Taheri <35961250+staheri14@users.noreply.github.com>
2025-03-21 10:26:30 -0700

    feat: extends `Comparison` with `DebugInfo` fields and makes `DebugInfo()` a method (#17)
    * extends Comparison struct with debug info related fields
    
    * unexports all fields of the Comparison struct
    
    * reverts some unwanted changes
    
    * further retraction
    
    * fixes an unwanted change

--- model/skipgraph/identifier.go
+++ model/skipgraph/identifier.go
@@ -20,51 +20,78 @@ type Identifier [IdentifierSizeBytes]byte
 // IdentifierList is a slice of Identifier
 type IdentifierList []Identifier
 
+type Comparison struct {
+	comparisonResult ComparisonResult // one of CompareEqual, CompareGreater, CompareLess
+	left, right      *Identifier      // the two identifiers being compared
+	diffIndex        uint32           // in case of inequality, the index of the first differing byte. 0-indexed.
+}
+
+// NewComparison creates a new Comparison instance.
+func NewComparison(result ComparisonResult, left, right *Identifier, diffIndex uint32) *Comparison {
+	return &Comparison{
+		comparisonResult: result,
+		left:             left,
+		right:            right,
+		diffIndex:        diffIndex,
+	}
+}
+
+// GetComparisonResult returns the comparison result.
+func (c *Comparison) GetComparisonResult() ComparisonResult {
+	return c.comparisonResult
+}
+
+// GetLeft returns the left identifier.
+func (c *Comparison) GetLeft() *Identifier {
+	return c.left
+}
+
+// GetRight returns the right identifier.
+func (c *Comparison) GetRight() *Identifier {
+	return c.right
+}
+
+// GetDiffIndex returns the index of the first differing byte.
+func (c *Comparison) GetDiffIndex() uint32 {
+	return c.diffIndex
+}
+
 // String converts Identifier to its hex representation.
-func (i Identifier) String() string {
+func (i *Identifier) String() string {
 	return hex.EncodeToString(i[:])
 }
 
 // Bytes returns the byte representation of an Identifier.
-func (i Identifier) Bytes() []byte {
+func (i *Identifier) Bytes() []byte {
 	return i[:]
 }
 
-type Comparison struct {
-	ComparisonResult ComparisonResult // one of CompareEqual, CompareGreater, CompareLess
-	DebugInfo        string           // in case of inequality, a human-readable debug info with the index of the first differing byte
-	DiffIndex        uint32           // in case of inequality, the index of the first differing byte
-}
-
 // DebugInfo returns a human-readable debug info for the comparison result.
-// diffIndex is the index of the first differing byte. In case of equality, diffIndex is not used.
-// diffIndex is 0-indexed.
-func DebugInfo(i Identifier, other Identifier, comparison ComparisonResult, diffIndex int) string {
-	switch comparison {
+func (c *Comparison) DebugInfo() string {
+	switch c.GetComparisonResult() {
 	case CompareGreater:
-		return fmt.Sprintf("%s > %s (at byte %d)", hex.EncodeToString(i[:diffIndex+1]), hex.EncodeToString(other[:diffIndex+1]), diffIndex)
+		return fmt.Sprintf("%s > %s (at byte %d)", hex.EncodeToString(c.GetLeft()[:c.GetDiffIndex()+1]), hex.EncodeToString(c.GetRight()[:c.GetDiffIndex()+1]), c.GetDiffIndex())
 	case CompareLess:
-		return fmt.Sprintf("%s < %s (at byte %d)", hex.EncodeToString(i[:diffIndex+1]), hex.EncodeToString(other[:diffIndex+1]), diffIndex)
+		return fmt.Sprintf("%s < %s (at byte %d)", hex.EncodeToString(c.GetLeft()[:c.GetDiffIndex()+1]), hex.EncodeToString(c.GetRight()[:c.GetDiffIndex()+1]), c.GetDiffIndex())
 	default:
-		return fmt.Sprintf("%s == %s", hex.EncodeToString(i[:diffIndex+1]), hex.EncodeToString(other[:diffIndex+1]))
+		return fmt.Sprintf("%s == %s", hex.EncodeToString(c.GetLeft()[:c.GetDiffIndex()+1]), hex.EncodeToString(c.GetRight()[:c.GetDiffIndex()+1]))
 	}
 }
 
 // Compare compares two Identifiers and returns a Comparison result, including the debugging info and the first mismatching byte index, if applicable.
-func (i Identifier) Compare(other Identifier) Comparison {
+func (i *Identifier) Compare(other *Identifier) Comparison {
 	for index := range i {
 		cmp := bytes.Compare(i[index:index+1], other[index:index+1])
 		switch cmp {
 		case 1:
-			return Comparison{CompareGreater, DebugInfo(i, other, CompareGreater, index), uint32(index)}
+			return Comparison{CompareGreater, i, other, uint32(index)}
 		case -1:
-
-			return Comparison{CompareLess, DebugInfo(i, other, CompareLess, index), uint32(index)}
+			return Comparison{CompareLess, i, other, uint32(index)}
 		default:
 			continue
 		}
 	}
-	return Comparison{CompareEqual, DebugInfo(i, other, CompareEqual, len(i)-1), uint32(len(i) - 1)}
+	return Comparison{CompareEqual, i, other, uint32(len(i) - 1)}
 }
 
 // ByteToId converts a byte slice b to an Identifier.
--- model/skipgraph/identifier_test.go
+++ model/skipgraph/identifier_test.go
@@ -17,17 +17,20 @@ func TestDebugInfo(t *testing.T) {
 	require.NoError(t, err)
 
 	// Test CompareGreater
-	debugInfo := skipgraph.DebugInfo(id2, id1, skipgraph.CompareGreater, 31)
+	c := skipgraph.NewComparison(skipgraph.CompareGreater, &id2, &id1, uint32(31))
+	debugInfo := c.DebugInfo()
 	expected := "0000000000000000000000000000000000000000000000000000000000010204 > 0000000000000000000000000000000000000000000000000000000000010203 (at byte 31)"
 	require.Equal(t, expected, debugInfo)
 
 	// Test CompareLess
-	debugInfo = skipgraph.DebugInfo(id1, id2, skipgraph.CompareLess, 31)
+	c = skipgraph.NewComparison(skipgraph.CompareLess, &id1, &id2, uint32(31))
+	debugInfo = c.DebugInfo()
 	expected = "0000000000000000000000000000000000000000000000000000000000010203 < 0000000000000000000000000000000000000000000000000000000000010204 (at byte 31)"
 	require.Equal(t, expected, debugInfo)
 
 	// Test CompareEqual
-	debugInfo = skipgraph.DebugInfo(id1, id1, skipgraph.CompareEqual, len(id1)-1)
+	c = skipgraph.NewComparison(skipgraph.CompareEqual, &id1, &id1, uint32(len(id1)-1))
+	debugInfo = c.DebugInfo()
 	expected = "0000000000000000000000000000000000000000000000000000000000010203 == 0000000000000000000000000000000000000000000000000000000000010203"
 	require.Equal(t, expected, debugInfo)
 }
@@ -41,42 +44,50 @@ func TestIdentifierCompare(t *testing.T) {
 	require.NoError(t, err)
 
 	// each id is equal to itself
-	require.Equal(t, skipgraph.DebugInfo(id0, id0, skipgraph.CompareEqual, len(id0)-1), id0.Compare(id0).DebugInfo)
-	require.Equal(t, skipgraph.DebugInfo(id1, id1, skipgraph.CompareEqual, len(id1)-1), id1.Compare(id1).DebugInfo)
-	require.Equal(t, skipgraph.DebugInfo(id2, id2, skipgraph.CompareEqual, len(id2)-1), id2.Compare(id2).DebugInfo)
+	exp := skipgraph.NewComparison(skipgraph.CompareEqual, &id0, &id0, uint32(len(id0)-1))
+	res := id0.Compare(&id0)
+	require.Equal(t, exp.DebugInfo(), res.DebugInfo())
+
+	exp = skipgraph.NewComparison(skipgraph.CompareEqual, &id1, &id1, uint32(len(id1)-1))
+	res = id1.Compare(&id1)
+	require.Equal(t, exp.DebugInfo(), res.DebugInfo())
+
+	exp = skipgraph.NewComparison(skipgraph.CompareEqual, &id2, &id2, uint32(len(id2)-1))
+	res = id2.Compare(&id2)
+	require.Equal(t, exp.DebugInfo(), res.DebugInfo())
 
 	// id0 < id1
-	comp := id0.Compare(id1)
-	require.Equal(t, skipgraph.CompareLess, comp.ComparisonResult)
-	require.Equal(t, "00 < 7f (at byte 0)", comp.DebugInfo)
-	require.Equal(t, uint32(0), comp.DiffIndex)
+	comp := id0.Compare(&id1)
+	require.Equal(t, skipgraph.CompareLess, comp.GetComparisonResult())
+	require.Equal(t, "00 < 7f (at byte 0)", comp.DebugInfo())
+	require.Equal(t, uint32(0), comp.GetDiffIndex())
 
-	comp = id1.Compare(id0)
-	require.Equal(t, skipgraph.CompareGreater, comp.ComparisonResult)
-	require.Equal(t, "7f > 00 (at byte 0)", comp.DebugInfo)
-	require.Equal(t, uint32(0), comp.DiffIndex)
+	comp = id1.Compare(&id0)
+	require.Equal(t, skipgraph.CompareGreater, comp.GetComparisonResult())
+	require.Equal(t, "7f > 00 (at byte 0)", comp.DebugInfo())
+	require.Equal(t, uint32(0), comp.GetDiffIndex())
 
 	// id1 < id2
-	comp = id1.Compare(id2)
-	require.Equal(t, skipgraph.CompareLess, comp.ComparisonResult)
-	require.Equal(t, "7f < ff (at byte 0)", comp.DebugInfo)
-	require.Equal(t, uint32(0), comp.DiffIndex)
+	comp = id1.Compare(&id2)
+	require.Equal(t, skipgraph.CompareLess, comp.GetComparisonResult())
+	require.Equal(t, "7f < ff (at byte 0)", comp.DebugInfo())
+	require.Equal(t, uint32(0), comp.GetDiffIndex())
 
-	comp = id2.Compare(id1)
-	require.Equal(t, skipgraph.CompareGreater, comp.ComparisonResult)
-	require.Equal(t, "ff > 7f (at byte 0)", comp.DebugInfo)
-	require.Equal(t, uint32(0), comp.DiffIndex)
+	comp = id2.Compare(&id1)
+	require.Equal(t, skipgraph.CompareGreater, comp.GetComparisonResult())
+	require.Equal(t, "ff > 7f (at byte 0)", comp.DebugInfo())
+	require.Equal(t, uint32(0), comp.GetDiffIndex())
 
 	// id0 < id2
-	comp = id0.Compare(id2)
-	require.Equal(t, skipgraph.CompareLess, comp.ComparisonResult)
-	require.Equal(t, "00 < ff (at byte 0)", comp.DebugInfo)
-	require.Equal(t, uint32(0), comp.DiffIndex)
+	comp = id0.Compare(&id2)
+	require.Equal(t, skipgraph.CompareLess, comp.GetComparisonResult())
+	require.Equal(t, "00 < ff (at byte 0)", comp.DebugInfo())
+	require.Equal(t, uint32(0), comp.GetDiffIndex())
 
-	comp = id2.Compare(id0)
-	require.Equal(t, skipgraph.CompareGreater, comp.ComparisonResult)
-	require.Equal(t, "ff > 00 (at byte 0)", comp.DebugInfo)
-	require.Equal(t, uint32(0), comp.DiffIndex)
+	comp = id2.Compare(&id0)
+	require.Equal(t, skipgraph.CompareGreater, comp.GetComparisonResult())
+	require.Equal(t, "ff > 00 (at byte 0)", comp.DebugInfo())
+	require.Equal(t, uint32(0), comp.GetDiffIndex())
 
 	// construct two random identifiers that differ only at index 16th (17th byte)
 	differingByteIndex := skipgraph.IdentifierSizeBytes / 2
@@ -94,20 +105,22 @@ func TestIdentifierCompare(t *testing.T) {
 	require.NoError(t, err)
 
 	// each identifier is equal to itself
-	require.Equal(t, skipgraph.CompareEqual, idRandomGreater.Compare(idRandomGreater).ComparisonResult)
-	require.Equal(t, skipgraph.CompareEqual, idRandomLess.Compare(idRandomLess).ComparisonResult)
+	c := idRandomGreater.Compare(&idRandomGreater)
+	require.Equal(t, skipgraph.CompareEqual, c.GetComparisonResult())
+	c = idRandomLess.Compare(&idRandomLess)
+	require.Equal(t, skipgraph.CompareEqual, c.GetComparisonResult())
 
-	comp = idRandomGreater.Compare(idRandomLess)
-	require.Equal(t, skipgraph.CompareGreater, comp.ComparisonResult)
-	require.Equal(t, uint32(differingByteIndex), comp.DiffIndex)
+	comp = idRandomGreater.Compare(&idRandomLess)
+	require.Equal(t, skipgraph.CompareGreater, comp.GetComparisonResult())
+	require.Equal(t, uint32(differingByteIndex), comp.GetDiffIndex())
 	expectedDebugInfo := fmt.Sprintf("%s > %s (at byte %d)", hex.EncodeToString(idRandomGreater[:differingByteIndex+1]), hex.EncodeToString(idRandomLess[:differingByteIndex+1]), differingByteIndex)
-	require.Equal(t, expectedDebugInfo, comp.DebugInfo)
+	require.Equal(t, expectedDebugInfo, comp.DebugInfo())
 
-	comp = idRandomLess.Compare(idRandomGreater)
-	require.Equal(t, skipgraph.CompareLess, comp.ComparisonResult)
-	require.Equal(t, uint32(differingByteIndex), comp.DiffIndex)
+	comp = idRandomLess.Compare(&idRandomGreater)
+	require.Equal(t, skipgraph.CompareLess, comp.GetComparisonResult())
+	require.Equal(t, uint32(differingByteIndex), comp.GetDiffIndex())
 	expectedDebugInfo = fmt.Sprintf("%s < %s (at byte %d)", hex.EncodeToString(idRandomLess[:differingByteIndex+1]), hex.EncodeToString(idRandomGreater[:differingByteIndex+1]), differingByteIndex)
-	require.Equal(t, expectedDebugInfo, comp.DebugInfo)
+	require.Equal(t, expectedDebugInfo, comp.DebugInfo())
 }
 func TestIdentifier_Bytes(t *testing.T) {
 	// 32 bytes of zero
